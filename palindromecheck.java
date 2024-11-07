e50a70e309698db63b35fcb5cc80a0b590c5f8e2
afrenk <afrenk@wpi.edu>
2024-11-07 12:10:02 -0500

    added base for palindromes

---
+++ palindromecheck.java
@@ -0,0 +1,33 @@
+public class palindromecheck {
+
+    string[] punct = {
+            ".", ",", ";", "?", "/", "!", ":", "'", "...", "(", ")", "-"};
+    // need to figure out how to add quotation marks to punct
+    public static boolean isPalindrome(String s, int i, int j) {
+        if (i >= j) {
+            return true;
+        }
+        if (s.charAt(i) != s.charAt(j)) {
+            return false;
+        }
+
+        return isPalindrome(i+1, j-1, A);
+    }
+    public static boolean isPalindrome(String s) {
+        return isPalindrome(s, 0, s.length()-1);
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java PalindromeCheck <word>");
+        }
+        else {
+            String word = args[0];
+            word = word.toLowerCase();
+            word = word.replaceAll(" ", "");
+            boolean isPalindrome = isPalindrome(word);
+            System.out.println(isPalindrome);
+
+        }
+    }
+}
