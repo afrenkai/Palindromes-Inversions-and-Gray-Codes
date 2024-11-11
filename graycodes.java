73a35beefa5b628e1fdfe1e1f202d61b5cad0385
afrenk <afrenk@wpi.edu>
2024-11-11 11:21:22 -0500

    updated docs

--- graycodes.java
+++ graycodes.java
@@ -84,15 +84,6 @@ public class graycodes {
     }
 }
 
-    // public static String[] reverse(String [] arr) {
-    //     for (int i = 0; i < arr.length/2; i++) {
-    //         String temp = arr[i];
-    //         arr[i] = arr[arr.length - i - 1];
-    //         arr[arr.length - i - 1] = temp;
-    //     }
-    //     return arr;
-    // }
-
 
 
 
